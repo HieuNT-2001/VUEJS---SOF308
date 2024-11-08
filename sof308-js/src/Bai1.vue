1f6438c08f01fa9e896e0cf62d89f5143bc1f729
HieuNT-2001-Student <hieuntpp03096@fpt.edu.vn>
2024-11-08 15:51:30 +0700

    lam lab2

---
+++ sof308-js/src/Bai1.vue
@@ -0,0 +1,39 @@
+<script setup>
+// BT1: Khai bao 2 biến fullname và studentCode;
+let fullname = 'Nguyễn Trung Hiếu'
+let studentCode = 'PP03096'
+
+// BT2: Sử dụng component Alert trong bootstrap
+// để hiển thị 2 biến fullname và studentCard
+
+// BT3: Khai báo 1 object myCar gồm các key: hangXe, mauXe, kieuXe,  tenXe, soChoNgoi trong thẻ Card
+let myCar = {
+  hangXe: 'HONDA',
+  mauXe: 'Trắng',
+  kieuXe: 'SUV',
+  tenXe: 'Lexus',
+  soChoNgoi: '5 chỗ ngồi',
+}
+</script>
+
+<template>
+  <!-- Hiển thị 2 biến fullname và studentCode -->
+  <div class="alert alert-success" role="alert">
+    <p>Fullname: {{ fullname }}</p>
+    <p>Student Code: {{ studentCode }}</p>
+  </div>
+
+  <!-- Hiển thị biến object myCar -->
+  <div class="card" style="width: 18rem">
+    <img src="https://m.baotuyenquang.com.vn/media/images/2022/11/img_20221125083953.jpeg" class="card-img-top"
+      alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ myCar.hangXe }}</h5>
+      <p>Màu xe: {{ myCar.mauXe }}</p>
+      <p>Kiểu xe: {{ myCar.kieuXe }}</p>
+      <p>Tên xe: {{ myCar.tenXe }}</p>
+      <p>Số chỗ ngồi: {{ myCar.soChoNgoi }}</p>
+      <a href="#" class="btn btn-primary">Mua</a>
+    </div>
+  </div>
+</template>
---
+++ sof308-js/src/Bai2.vue
@@ -0,0 +1,47 @@
+<!-- Code JS nam trong the script -->
+<script setup>
+const products = [
+  {
+    name: "Sản phẩm 1",
+    description: "Mô tả sản phẩm 1, rất chất lượng và đáng mua.",
+    price: 500000,
+    image: "http://picsum.photos/id/1/300/300",
+  },
+  {
+    name: "Sản phẩm 2",
+    description: "Mô tả sản phẩm 2, chất lượng tốt và giá phải chăng.",
+    price: 300000,
+    image: "http://picsum.photos/id/2/300/300",
+  },
+  {
+    name: "Sản phẩm 3",
+    description: "Mô tả sản phẩm 3, sản phẩm cao cấp và nhiều tính năng.",
+    price: 1000000,
+    image: "http://picsum.photos/id/3/300/300",
+  }
+];
+</script>
+
+<!-- Code HTML nam trong the template -->
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center">Danh sách sản phẩm</h1>
+    <div class="row">
+      <!-- Sản phẩm 1 -->
+      <div class="col-sm-4" v-for="product in products" :key="product.name">
+        <div class="card">
+          <img :src="product.image" alt="Hình ảnh sản phẩm" class="card-img-top" />
+          <div class="card-body">
+            <h3 class="card-title">{{ product.name }}</h3>
+            <p class="card-text">{{ product.description }}</p>
+            <p class="card-text">Giá: {{ product.price }} VND</p>
+            <button class="btn btn-success">Mua ngay</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Code CSS nam trong the style -->
+<style scoped></style>
